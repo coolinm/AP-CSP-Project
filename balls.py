09bd4ccca828e20732ff65aa99d891c8122650b4
Colin Metcalf <colinmetcalf@outlook.com>
2023-03-25 21:54:44 -0700

    Update balls.py

--- balls.py
+++ balls.py
@@ -1,14 +1,14 @@
-# Import and initialize the pygame library
+# import and initialize the pygame library
 import pygame
 import time
 pygame.init()
 
-# Set up the screen and clock
+# set up the screen and clock
 screen = pygame.display.set_mode([600, 650])
 clock = pygame.time.Clock()
 startTime = time.time()
 
-#create text fonts
+# create text fonts
 font = pygame.font.Font('freesansbold.ttf', 32)
 font_sm = pygame.font.Font('freesansbold.ttf', 16)
  
@@ -24,12 +24,12 @@ textRect2 = text2.get_rect()
 textRect1.center = (600 // 2, 650 // 2)
 textRect2.center = (600 // 2, 650 // 1.3)
 
-# Run until the user asks to quit
+# run until the user asks to quit
 running = True
 started = False
 while running:
 
-    # Check if window has been asked to close
+    # check if window has been asked to close
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -39,18 +39,18 @@ while running:
             if event.key == pygame.K_ESCAPE:
                 running = False
 
-    # Fill the background with white
+    # fill the background with white
     screen.fill((255, 255, 255))
 
-    # Check if the start screen has been passed
+    # check if the start screen has been passed
     while started == False:
-        # Render text
+        # render text
         screen.fill((255, 255, 255))
         screen.blit(text1, textRect1)
         screen.blit(text2, textRect2)
         pygame.display.update()
 
-        # Check if mouse has been clicked and start game if it has
+        # check if mouse has been clicked and start game if it has
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -66,7 +66,7 @@ while running:
 
         clock.tick(60)
 
-    # Draw game borders and lines
+    # draw game borders and lines
     pygame.draw.line(screen, (0,0,0), (600, 0), (600, 600), 10 )
     pygame.draw.line(screen, (0,0,0), (0,600), (0,0), 10)
     pygame.draw.line(screen, (0,0,0), (600,600), (0,600), 10)
@@ -76,19 +76,20 @@ while running:
     pygame.draw.line(screen, (0,0,0), (0, 200), (600,200), 10)
     pygame.draw.line(screen, (0,0,0), (0,400), (600,400), 10)
 
-    # Calculate how many seconds have passed since the game has started
-    # and create a text object containing it
+    # calculate seconds passed since game start
+    # and create text object
     seconds = round(time.time() - startTime, 1)
     timerText = font.render(str(seconds), True, (0,0,0), (255,255,255))
     timerRect = timerText.get_rect()
     timerRect.center = (600 // 11, 650 // 1.035)
     screen.blit(timerText, timerRect)
-    # Update the display
+
+    # update the display
     pygame.display.update()
 
     clock.tick(60)
 
-# Done! Time to quit.
+# close game
 pygame.quit()
 
 # def playerMove(x, y):
